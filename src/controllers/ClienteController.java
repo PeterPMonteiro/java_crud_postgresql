4adb18bfb66896ecd78c7d2faff922c2ce84bf6c
PeterPMonteiro <peterxmonteiro@gmail.com>
2022-10-26 12:55:39 -0300

    Finalizando do CRUD de clientes

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.List;
+
 import entities.Cliente;
 import helpers.ClienteHelper;
 import repositories.ClienteRepository;
@@ -18,14 +20,96 @@ public class ClienteController {
 			cliente.setNome(ClienteHelper.lerNome());
 			cliente.setEmail(ClienteHelper.lerEmail());
 			cliente.setTelefone(ClienteHelper.lerTelefone());
-			
+
 			ClienteRepository clienteRepository = new ClienteRepository();
 			clienteRepository.create(cliente);
-			
+
 			System.out.println("\nCliente cadastrado com sucesso!!");
 
 		} catch (Exception e) {
 			System.out.println("\nErro:" + e.getMessage());
 		}
 	}
+	// metodo para realizar a edição de um cliente
+
+	public void atualizarCliente() {
+
+		try {
+			System.out.println("\nAtualização de Cliente");
+
+			Integer idCliente = ClienteHelper.lerIdCliente();
+
+			ClienteRepository clienteRepository = new ClienteRepository();
+			Cliente cliente = clienteRepository.findById(idCliente);
+
+			if (cliente == null) {
+				System.out.println("\nCliente não encontrado,verifique o ID do cliente.");
+			} else {
+				cliente.setNome(ClienteHelper.lerNome());
+				cliente.setEmail(ClienteHelper.lerEmail());
+				cliente.setTelefone(ClienteHelper.lerEmail());
+
+				clienteRepository.update(cliente);
+
+				System.out.println("\nCliente atualizado com sucesso.");
+
+			}
+		}
+
+		catch (Exception e) {
+			System.out.println("\nErro: " + e.getMessage());
+		}
+	}
+
+	// metodo para realizar exlusao de cliente
+	public void excluirCliente() {
+
+		try {
+			System.out.println("\nExclusão de Cliente\n");
+
+			Integer idCliente = ClienteHelper.lerIdCliente();
+
+			ClienteRepository clienteRepository = new ClienteRepository();
+			Cliente cliente = clienteRepository.findById(idCliente);
+
+			if (cliente == null) {
+				System.out.println("\nCliente não encontrado, verifique o Id informado.");
+			} 
+			
+			else {
+				clienteRepository.delete(cliente);
+				System.out.println("\nCliente excluido com sucesso!");
+				
+			}
+		} catch (Exception e) {
+			System.out.println("\nErro:" + e.getMessage());
+		}
+	}
+	
+		// metodo para consultar e imprimir todos os clientes cadastrados
+	public void consultarClientes() {
+		
+		try {
+			System.out.println("\nConsulta de Clientes\n");
+			ClienteRepository clienteRepository = new ClienteRepository();
+			List<Cliente> lista = clienteRepository.findAll();
+			
+			System.out.println("\nTotal de clientes:" + lista.size());
+			
+			//FOREACH (para cada..)
+			for(Cliente cliente : lista) {
+				
+				System.out.println("\nCLIENTE: ");
+				System.out.println("ID.......: " + cliente.getIdCliente());
+				System.out.println("NOME.....: " + cliente.getNome());
+				System.out.println("EMAIL....: " + cliente.getEmail());
+				System.out.println("TELEFONE.: " + cliente.getTelefone());
+			}
+	 	}
+		catch(Exception e) {
+			System.out.println("\nErro:" + e.getMessage());
+		}
+	}
+		
 }
+
--- src/repositories/ClienteRepository.java
+++ src/repositories/ClienteRepository.java
@@ -2,6 +2,8 @@ package repositories;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.Cliente;
@@ -31,23 +33,93 @@ public class ClienteRepository {
 
 	// metodo para atualizar um cliente no banco de dados
 	public void update(Cliente cliente) throws Exception {
-		// TODO
+
+		// abrir conexao com o banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// escrever um comando sql para executar o banco de dados
+		PreparedStatement statement = connection
+				.prepareStatement("update cliente set nome=?, email=? telefone=? where idcliente=?");
+		statement.setString(1, cliente.getNome());
+		statement.setString(2, cliente.getEmail());
+		statement.setString(3, cliente.getTelefone());
+		statement.setInt(4, cliente.getIdCliente());
+
+		// fechando conexao com banco de dados
+		connection.close();
+
 	}
 
 	// metodo para excluir um cliente no banco de dados
 	public void delete(Cliente cliente) throws Exception {
-		// TODO
+
+		// abrir conexao com banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// escrevendo um comando sql para executar no banco de dados
+		PreparedStatement statement = connection.prepareStatement("delete from cliente where idcliente=?");
+		statement.setInt(1, cliente.getIdCliente());
+		statement.execute();
+
+		// fechando conexão
+		connection.close();
 	}
 
 	// metodo para consultar todos os clientes no banco de dados
 	public List<Cliente> findAll() throws Exception {
-		// TODO
-		return null;
+		// abrir conexao com banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// escrever um comando SQL para executar no banco de dados
+		PreparedStatement statement = connection.prepareStatement("select * from cliente");
+		ResultSet resultSet = statement.executeQuery();
+
+		// Criando uma lista de Clientes Vazia
+		List<Cliente> lista = new ArrayList<Cliente>();
+
+		// percorrer cada registro contido no ResultSet
+		// enquanto houver registro, leia
+		while (resultSet.next())
+		{
+			Cliente cliente = new Cliente();
+
+			cliente.setIdCliente(resultSet.getInt("idcliente"));
+			cliente.setNome(resultSet.getString("nome"));
+			cliente.setEmail(resultSet.getString("email"));
+			cliente.setTelefone(resultSet.getString("telefone"));
+
+			lista.add(cliente); // adicionando cada cliente dentro da lista
+		}
+
+		connection.close();// fechando conexao
+
+		return lista;
 	}
 
 	// metodo para consultar 1 cliente no banco de dados pelo ID
 	public Cliente findById(Integer idCliente) throws Exception {
-		// TODO
-		return null;
+		// abrir conexao com banco de dados
+		Connection connection = ConnectionFactory.createConnection();
+
+		// escrevendo um comando sql para consultar no banco de dados
+		PreparedStatement statement = connection.prepareStatement("select * from cliente where idcliente=?");
+		statement.setInt(1, idCliente);
+		ResultSet resultSet = statement.executeQuery();
+
+		Cliente cliente = null;
+
+		// se algum cliente foi encontrado
+		if (resultSet.next()) {
+			
+			cliente = new Cliente();
+			
+			cliente.setIdCliente(resultSet.getInt("idcliente"));
+			cliente.setNome(resultSet.getString("nome"));
+			cliente.setEmail(resultSet.getString("email"));
+			cliente.setTelefone(resultSet.getString("telefone"));
+
+		}
+		connection.close(); // fechando conexao com o bando de dados
+		return cliente;// retornando o cliente
 	}
 }
